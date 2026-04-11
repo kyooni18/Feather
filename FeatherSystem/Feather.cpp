d674b4e3b6f0ecb80540820d39549dfb76620c0b
kyooni18 <kyoung01122189@gmail.com>
2026-04-11 17:20:45 +0900

    added wrapper to add tasks

--- FeatherSystem/Feather.cpp
+++ FeatherSystem/Feather.cpp
@@ -5,3 +5,13 @@ Feather::Feather():
     clock([]() -> uint64_t { return 0; }),
     scheduler(clock) {
 }
+
+void Feather::InstantTask(void (*task_to_run)(...), uint8_t priority) {
+    scheduler.add_instant_task(task_to_run, priority);
+}
+void Feather::DefferedTask(void (*task_to_run)(...), uint64_t time_to_run, uint8_t priority) {
+    scheduler.add_deferred_task(task_to_run, time_to_run, priority);
+}
+void Feather::PeriodicTask(void (*task_to_run)(...), uint64_t time_to_run, uint32_t repeat_cycle, uint8_t priority) {
+    scheduler.add_periodic_task(task_to_run, time_to_run, repeat_cycle, priority);
+}
--- FeatherSystem/Feather.hpp
+++ FeatherSystem/Feather.hpp
@@ -9,4 +9,8 @@ class Feather {
 
     public:
     Feather();
+
+    void InstantTask(void (*task_to_run)(...), uint8_t priority);
+    void DefferedTask(void (*task_to_run)(...), uint64_t time_to_run, uint8_t priority);
+    void PeriodicTask(void (*task_to_run)(...), uint64_t time_to_run, uint32_t repeat_cycle, uint8_t priority);
 };
