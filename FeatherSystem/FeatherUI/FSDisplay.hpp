e8a1bd6f8d327458a3b19940d83c7f2aa7e8789c
kyooni18 <kyoung01122189@gmail.com>
2026-04-16 18:13:42 +0900

    FSDisplay in progress...

---
+++ FeatherSystem/FeatherUI/FSDisplay.hpp
@@ -0,0 +1,15 @@
+#include "../Feather.hpp"
+#include <vector>
+
+class FSDisplay {
+	Feather* feather;
+
+	
+
+	public:
+	
+
+	FSDisplay(Feather* feathersrc) {
+		this->feather = feathersrc;
+	}
+};
